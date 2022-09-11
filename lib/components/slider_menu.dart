eef0cf042668cd6fbec93305b34f0a47e095bab6
ashdik <ashdik.saxophone@gmail.com>
2022-09-12 01:33:35 +0900

    feat: Make _SliderMenu public

---
+++ lib/components/slider_menu.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SliderMenu extends StatelessWidget {
+  const SliderMenu({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.min,
+    required this.max,
+    required this.divisions,
+    required this.onChanged,
+  });
+
+  final String label;
+  final double value;
+  final double min;
+  final double max;
+  final int divisions;
+
+  final ValueChanged<double> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          label,
+          style: theme.textTheme.headline6!.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(min.toString(), style: theme.textTheme.caption),
+            Text(
+              value.toString(),
+              style: theme.textTheme.titleLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(max.toString(), style: theme.textTheme.caption),
+          ],
+        ),
+        Slider.adaptive(
+          value: value,
+          min: min,
+          max: max,
+          divisions: divisions,
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+}
